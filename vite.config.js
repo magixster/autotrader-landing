94a53d089fac9efa5b1549af37b73ec9018ab2c2
sachinn <sachinn@MacBookPro.lan>
2026-05-27 22:23:45 +0530

    info added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Automatically sets base path to the subfolder for production builds
+  // This tells Vite to prepend your repository name to ALL compiled assets in production
   base: process.env.NODE_ENV === 'production' ? '/autotrader-landing/' : '/',
 })
